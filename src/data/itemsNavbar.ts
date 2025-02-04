23da1493e25cc65db8faf6d25e44ae31432720ec
felipe <jonathanfelipe0111@hotmail.com>
2025-02-04 11:31:40 -0500

    Basic project configuration

---
+++ src/data/itemsNavbar.ts
@@ -0,0 +1,27 @@
+export const itemsNavbar = [
+    {
+        name: "Inicio",
+        link: "/",
+        icon: "Home",
+    },
+    {
+        name: "Trucos",
+        link: "/trucos",
+        icon: "LightningBolt",
+    },
+    {
+        name: "Competiciones",
+        link: "/competiciones",
+        icon: "Trophy",
+    },
+    {
+        name: "Parches",
+        link: "/parches",
+        icon: "Wrench", 
+    },
+    {
+        name: "Tutoriales",
+        link: "/tutoriales",
+        icon: "BookOpen", 
+    },
+]
